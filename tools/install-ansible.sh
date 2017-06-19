6c594c2fc16b718982a829454bc114d838ea88ef
Mark Tompsett <mark_tompsett@sil.org>
2017-06-19 17:42:44 -0400

    Issue 200: Stretch VM fails to come up nicely
    By changing the script to attempt installing ansible from default repos,
    then adding only if the version is less than 1.9, this allows stretch
    to come up with ansible 2.2 which isn't 2.3 from the repo, but more than
    the 1.9 which is supposedly the minimum based on the README file.
    
    TEST PLAN
    ----------
    vagrant destroy jessie
    vagrant up jessie
    vagrant destroy xenial
    vagrant up xenial
    vagrant destroy stretch
    vagrant up stretch
    -- all should come up without issue.

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
-sudo apt-get install -q -y software-properties-common
-sudo apt-add-repository -y "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main"
 sudo apt-get update -q
 sudo apt-get install -y -q ansible
+ANSIBLE_VERSION=`dpkg -s ansible | grep Version | cut -f2 -d' '`
+if [[ "$ANSIBLE_VERSION" < "1.9" ]]; then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
+    sudo apt-get install -q -y software-properties-common
+    sudo apt-add-repository -y "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main"
+    sudo apt-get update -q
+    sudo apt-get install -y -q ansible
+fi
