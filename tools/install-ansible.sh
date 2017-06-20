4e95df27bc13c89d204c7c5036c08e3bf649562b
Mark Tompsett <mark_tompsett@sil.org>
2017-06-19 22:22:13 -0400

    Issue 200: download deb from url requires 2.1
    http://docs.ansible.com/ansible/apt_module.html
    documents that :// in the path downloads from a URL as of
    version 2.1, and this functionality is currently needed
    for JSON::Validator

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -q
 sudo apt-get install -y -q ansible
 ANSIBLE_VERSION=`dpkg -s ansible | grep Version | cut -f2 -d' '`
-if [[ "$ANSIBLE_VERSION" < "1.9" ]]; then
+if [[ "$ANSIBLE_VERSION" < "2.1" ]]; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
     sudo apt-get install -q -y software-properties-common
     sudo apt-add-repository -y "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main"
