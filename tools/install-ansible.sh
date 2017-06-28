4fdd38ddd0bc57be05d02f781e37f0be2ea6462d
Mark Tompsett <mark_tompsett@sil.org>
2017-06-28 10:30:36 -0400

    Issue #200: tweak out double install of ansible
    As per Tomas' request, this conditionally adds the repo
    and installs outside the if.

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -q
-sudo apt-get install -y -q ansible
-ANSIBLE_VERSION=`dpkg -s ansible | grep Version | cut -f2 -d' '`
+ANSIBLE_VERSION=`apt-cache show ansible | grep Version | cut -f2 -d' '`
 if [[ "$ANSIBLE_VERSION" < "2.1" ]]; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
     sudo apt-get install -q -y software-properties-common
     sudo apt-add-repository -y "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main"
     sudo apt-get update -q
-    sudo apt-get install -y -q ansible
 fi
+sudo apt-get install -y -q ansible
