c753743c3827c55301f30ff6befaf62c872d3055
Mark Tompsett <mark_tompsett@sil.org>
2017-06-28 11:53:42 -0400

    Issue #200: Fix multiple versions available for xenial
    By default Xenial was 2.0, but also had access to 2.1
    but the 2.0 was installed, because the version number
    wasn't updated.
    
    Stretch does 2.2 by default.
    
    Jessie is archaic default at 1.7, so ppa is used.

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update -q
-ANSIBLE_VERSION=`apt-cache show ansible | grep Version | cut -f2 -d' '`
+ANSIBLE_VERSION=`apt-cache show ansible | grep Version | cut -f2 -d' ' | sort -r | head -1`
 if [[ "$ANSIBLE_VERSION" < "2.1" ]]; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7BB9C367
     sudo apt-get install -q -y software-properties-common
     sudo apt-add-repository -y "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main"
     sudo apt-get update -q
+    ANSIBLE_VERSION=`apt-cache show ansible | grep Version | cut -f2 -d' ' | sort -r | head -1`
 fi
-sudo apt-get install -y -q ansible
+sudo apt-get install -y -q ansible=$ANSIBLE_VERSION
